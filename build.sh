2c8ee6e5424f06075bd0ca8fa88d58030cbdb7ea
Rafael Oliveira <rafael@curiosity.ai>
2026-06-09 23:23:04 +0200

    fix build

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh -InstallDir ./dotnet
+export PATH="$PATH:/opt/buildhome/.dotnet/tools"
 ./dotnet/dotnet --version
-./dotnet/dotnet publish -c Release -o output
 ./dotnet/dotnet tool install --global h5-compiler
 ./dotnet/dotnet build app/LlmPrices.csproj -c Release
